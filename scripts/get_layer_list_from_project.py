67118b744da69322d6196d4f2b1b15c312aa4963
TPiergiovanni <tpiergiovanni@ville-suresnes.fr>
2021-01-20 09:22:01 +0100

    working on script to get mxd details

--- scripts/get_layer_list_from_project.py
+++ scripts/get_layer_list_from_project.py
@@ -1,39 +1,45 @@
 # Get all layer within .mxd project
+# Use ArcGis Python console.
+# Project mxd must be loaded n Arc Map
 
 import arcpy
 import os
 import csv
 
 
-# Input .mxd directory location
-PATH2 = r"C:\03_sig"
+# Define input .mxd directory location
+PATH2 = r"C:\01_sig\mxd"
 
-# Output file and directory
-OUTPUT_PATH = r"C:\03_sig"
+# Define input .mxd directory file
+mxd_file = "inventaire.mxd"
+
+# Define output directory
+OUTPUT_PATH = r"C:\01_sig\output"
+
+# Define output file name
 output_file = os.path.join(OUTPUT_PATH, 'layers.csv')
 
 
 arcpy.env.workspace = PATH2
-arcpy.env.overwriteOutput=True
+arcpy.env.overwriteOutput = True
 layers_list = []
 
 
-for file in arcpy.ListFiles("inventaire.mxd"):
+for file in arcpy.ListFiles(mxd_file):
 	mxd_path = os.path.join(PATH2,file)
 	mxd = arcpy.mapping.MapDocument(mxd_path) 
 	layers = arcpy.mapping.ListLayers(mxd)
 	for layer in layers:
 		if layer.supports("DATASOURCE"):
 			fields = arcpy.ListFields(layer)
-			for field in fields:
-				layer_info = {
+			layer_info = {
 					"layer_name": layer.name.encode('utf-8'),
 					"layer_source": layer.dataSource.encode('utf-8'),
-					"layer_field_name": field.name.encode('utf-8'),
-					"layer_field_type": field.type.encode('utf-8')}
-				layers_list.append(layer_info)
-
-
+					"layer_attributes": []}
+			for field in fields:
+				layer_info["layer_attributes"].append(
+						(field.name.encode('utf-8'),field.type.encode('utf-8')))
+			layers_list.append(layer_info)
 
 	with open(output_file, 'wb') as csvfile:
 		filewriter = csv.writer(
@@ -44,6 +50,5 @@ for file in arcpy.ListFiles("inventaire.mxd"):
 			filewriter.writerow(
 					[layer["layer_name"],
 					layer["layer_source"],
-					layer["layer_field_name"],
-					layer["layer_field_type"]])
+					layer["layer_attributes"]])
 		
